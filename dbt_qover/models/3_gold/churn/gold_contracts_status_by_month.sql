47843ec897fe28591ba0e61e8b2f7cc402d331a7
Robin <r.chesne@gmail.com>
2023-07-16 18:56:02 +0200

    metrics

--- dbt_qover/models/3_gold/churn/gold_contracts_status_by_month.sql
+++ dbt_qover/models/3_gold/churn/gold_contracts_status_by_month.sql
@@ -11,19 +11,21 @@ with source as (
 , crossjoined as (
   select
     dates.year_month
-    , first_day_of_month
     , source.*
   from source
   cross join dates
   where
     source.start_date <= dates.full_date
     and coalesce(source.end_date, date_trunc(cast(current_timestamp() as date), month)) >= full_date
-  group by 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+  group by 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
 )
 
 , add_prev_period_was_present as (
     select *
-         , case when lag(year_month) over(partition by contract_id order by year_month) is null then false else true end as was_present_prev_period
+         , case when 
+             lag(year_month, 1) over(partition by contract_id order by year_month) is null then false 
+             else true end 
+           as was_present_prev_period
     from crossjoined
 )
 
@@ -31,13 +33,13 @@ with source as (
   select
     *
     , coalesce(format_date('%Y-%m', end_date) = year_month, false) as has_churned_this_period
-  from prev_period_was_present
+  from add_prev_period_was_present
 )
 
 , reorder_cols as (
   select
     year_month
-    , first_day_of_month
+    , was_present_prev_period
     , has_churned_this_period
     , contract_id
     , policyholder_id
