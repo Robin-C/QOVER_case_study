55219604612418906842226185a9ca770c1d34df
Robin <r.chesne@gmail.com>
2023-07-17 15:27:02 +0200

    cs metrics

--- dbt_qover/models/2_stg/facts/stg_fact_tickets_ordered_1.sql
+++ dbt_qover/models/2_stg/facts/stg_fact_tickets_ordered_1.sql
@@ -10,9 +10,23 @@ with source as (
     , contract_id
     , row_number() over (partition by contract_id order by ticket_id) as ticket_number
     , duration_day
-    , {{ tickets_duration_brackets('duration_day', false) }} as duration_day_bracket
     , type
   from source
+),
+
+clean_duration as (
+  select ticket_id
+  , contract_id
+  , ticket_number
+  , case when duration_day < 0 then 0 else duration_day end as duration_day
+  , type
+  from add_step_number
+),
+
+add_duration_bracket as (
+  select *
+      , {{ tickets_duration_brackets('duration_day', false) }} as duration_day_bracket
+  from clean_duration
 ),
 
 cleaning_type as (
@@ -22,7 +36,7 @@ cleaning_type as (
   , duration_day
   , duration_day_bracket
   , case when type = 'claims' then 'claim' else type end as type
-  from add_step_number
+  from add_duration_bracket
 )
 
 , final as (
